ab0abf3934b8ea8bb7bd37032c2507eb4b00c55e
KochetkovAlex <leshakochetcov@gmail.com>
2025-08-17 14:39:01 +0300

    create README.md file. Add new database(but like a comment)

--- backend/database/model.py
+++ backend/database/model.py
@@ -1,6 +1,8 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import select
+from sqlalchemy.orm         import DeclarativeBase, Mapped, mapped_column
+from datetime               import datetime
+from sqlalchemy             import select, func
+
 
 engine = create_async_engine('sqlite+aiosqlite:///tasks.db')
 
@@ -17,12 +19,21 @@ class TaskModel(Base):
     title: Mapped[str]
     tag: Mapped[str]
     description: Mapped[str]
+    # created_at: Mapped[datetime] = mapped_column(server_default=func.now())
 
+# TODO: Реализуй механизм перевода активной задачи в список выполненных и добавь отображение выполненных.
+# Скорее всего тебе понадобится сделать на js функцию перерисовки страницы после нажатия кнопки ADD или кнопки "Done"(рабочее название)
+"""
+class CompletedTaskModel(Base):
+    __tablename__ = "completed_tasks"
 
-class CompletedTaskModel(TaskModel):
-    __tablename__ = "completed_task"
-
-
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(nullable=False)
+    tag: Mapped[str] = mapped_column(nullable=False)
+    description: Mapped[str] = mapped_column(nullable=True)
+    created_at: Mapped[datetime] = mapped_column(nullable=False)
+    completed_at: Mapped[datetime] = mapped_column(nullable=False)
+"""
 # async func for getting session
 async def get_session():
     async with new_session() as session:
